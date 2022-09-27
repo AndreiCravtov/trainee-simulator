270f2cbecd4767f4c759e207d41cd26c4d3ac25d
lucacooperlindsay <lcooper-lindsay@spartaglobal.com>
2022-09-27 16:15:01 +0100

    TrainingCentres removed use of courses enum

--- src/main/java/com/sparta/main/model/TrainingCentre.java
+++ src/main/java/com/sparta/main/model/TrainingCentre.java
@@ -1,4 +1,24 @@
 package com.sparta.main.model;
 
+import java.util.ArrayList;
+
 public class TrainingCentre {
+    private final int centerID;
+    ArrayList<Trainee> newTrainee = new ArrayList<>();
+
+    public TrainingCentre(int centerID) {
+        this.centerID = centerID;
+    }
+
+    public void addTrainee(Trainee trainee){
+        newTrainee.add(trainee);
+    }
+
+    public int getCenterID() {
+        return centerID;
+    }
+
+    public ArrayList<Trainee> getTrainee() {
+        return newTrainee;
+    }
 }
