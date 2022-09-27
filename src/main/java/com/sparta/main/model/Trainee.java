2d3747e70e53c13894d40af15dbba425facdf0b9
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-09-27 16:02:19 +0100

    trainee changes

--- src/main/java/com/sparta/main/model/Trainee.java
+++ src/main/java/com/sparta/main/model/Trainee.java
@@ -1,4 +1,15 @@
 package com.sparta.main.model;
 
 public class Trainee {
+    private final int traineeId;
+    private final Course course;
+
+    public int getId() { return traineeId; }
+
+    public Course getCourse() { return course; }
+
+    public Trainee (int id, Course course) {
+        this.traineeId = id;
+        this.course = course;
+    }
 }
