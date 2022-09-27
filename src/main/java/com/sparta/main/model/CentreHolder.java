b9a5fbaea5c755222114ab444c71843849b83d38
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-27 16:40:48 +0100

    Added the assign trainees method to the CentreHolder class

--- src/main/java/com/sparta/main/model/CentreHolder.java
+++ src/main/java/com/sparta/main/model/CentreHolder.java
@@ -1,14 +1,14 @@
 package com.sparta.main.model;
 
 import java.util.ArrayList;
-import java.util.Queue;
 
 public class CentreHolder {
 
     ArrayList<TrainingCentre> centres = new ArrayList<>();
 
-    public void assignTrainees(TrainingCentre tc, Queue<Trainee> traineeQueue) {
-        tc.addTrainee(traineeQueue.remove());
+    public void assignTrainees() {
+        // TrainingCentre tc = class.findAvalaibleCentre();
+        // tc.addTrainee(WaitingList.queue.remove());
     }
 
 
