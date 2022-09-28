4cbf2c085aa6bf66e124d365313f9b4fa99f9d42
Kilmurray <66964622+MaddyKilmurray@users.noreply.github.com>
2022-09-28 11:49:52 +0100

    Waiting List - Deque implemented

--- src/test/java/model/WaitingListTest.java
+++ src/test/java/model/WaitingListTest.java
@@ -3,6 +3,8 @@ package model;
 import com.sparta.main.model.Course;
 import com.sparta.main.model.Trainee;
 import com.sparta.main.model.WaitingList;
+import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,24 +34,53 @@ public class WaitingListTest {
     }
 
     @Test
-    public void waitingList_addTrainees() {
+    public void waitingList_addTraineeAtEnd() {
         testWaitingList = WaitingList.getInstance();
         int startSize = testWaitingList.sizeOfWaitingList();
-        testWaitingList.addTrainee(testTrainee1);
+        Assertions.assertTrue(testWaitingList.addTraineeAtEnd(testTrainee1));
         int afterSize = testWaitingList.sizeOfWaitingList();
         Assertions.assertEquals(startSize+1,afterSize);
     }
 
     @Test
-    public void waitingList_getFirstInQueue() {
+    public void waitingList_getFirstInQueue_OneTraineeAtEnd() {
         testWaitingList = WaitingList.getInstance();
-        testWaitingList.addTrainee(testTrainee1);
+        testWaitingList.addTraineeAtEnd(testTrainee1);
         Assertions.assertEquals(testTrainee1, testWaitingList.getFirstInQueue());
+    }
+
+    @Test
+    public void waitingList_addTraineeAtStart() {
+        testWaitingList = WaitingList.getInstance();
+        int startSize = testWaitingList.sizeOfWaitingList();
+        Assertions.assertTrue(testWaitingList.addTraineeAtStart(testTrainee1));
+        int afterSize = testWaitingList.sizeOfWaitingList();
+        Assertions.assertEquals(startSize+1,afterSize);
+    }
+
+    @Test
+    public void waitingList_getFirstInQueue_OneTraineeAtStart() {
+        testWaitingList = WaitingList.getInstance();
+        testWaitingList.addTraineeAtStart(testTrainee1);
+        Assertions.assertEquals(testTrainee1, testWaitingList.getFirstInQueue());
+    }
+
+    @Test
+    public void waitingList_getFirstInQueue_MultipleTrainees() {
+        testWaitingList = WaitingList.getInstance();
+        testWaitingList.addTraineeAtEnd(testTrainee1);
+        testWaitingList.addTraineeAtStart(testTrainee2);
+        Assertions.assertEquals(testTrainee2, testWaitingList.getFirstInQueue());
     }
 
     @Test
     public void waitingList_getFirstInQueue_emptyQueue() {
         testWaitingList = WaitingList.getInstance();
         Assertions.assertNull(testWaitingList.getFirstInQueue());
+    }
+
+    @AfterEach
+    public void setdown() {
+        testWaitingList = null;
     }
 }
