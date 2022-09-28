36c66b2754206e1621568c57f1494fe68647feeb
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-28 11:55:28 +0100

    added some tests

--- src/test/java/com/sparta/main/model/CentreHolderTest.java
+++ src/test/java/com/sparta/main/model/CentreHolderTest.java
@@ -1,5 +1,6 @@
 package com.sparta.main.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,10 @@ class CentreHolderTest {
     @Test
     @DisplayName("Functional Test 1")
     void test1() {
+        CentreHolder holder = CentreHolder.getInstance();
+        TrainingCentre tc = new TrainingCentre(6969);
+        holder.addToHolder(tc);
+        Assertions.assertEquals(1, CentreHolder.getCentres().size());
 
     }
 
