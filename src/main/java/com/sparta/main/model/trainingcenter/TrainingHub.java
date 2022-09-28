fcf06ff8349764d78b7ad53c1b257eb76f077263
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-09-28 14:54:50 +0100

    training hub update

--- src/main/java/com/sparta/main/model/trainingcenter/TrainingHub.java
+++ src/main/java/com/sparta/main/model/trainingcenter/TrainingHub.java
@@ -1,24 +1,29 @@
 package com.sparta.main.model.trainingcenter;
 
+import com.sparta.main.model.Timeable;
 import com.sparta.main.model.Trainee;
 
 public class TrainingHub extends TrainingCentre {
-    public TrainingHub(int centerID) {
-        super(centerID);
+    public TrainingHub(int id, Timeable timeable) {
+        super(id, timeable);
     }
 
     @Override
     public boolean canBeClosed() {
-        return false;
+        return (timekeeper.getTime() - timeCreated) > GRACE_PERIOD &&
+                trainees.size() < 25;
     }
 
     @Override
     public boolean canAdd(Trainee trainee) {
-        return false;
+        return trainee != null &&
+                trainees.size() < 100 &&
+                !trainees.contains(trainee);
     }
 
     @Override
     public boolean addTrainee(Trainee trainee) {
-        return false;
+        if (!canAdd(trainee)) return false;
+        return trainees.add(trainee);
     }
 }
