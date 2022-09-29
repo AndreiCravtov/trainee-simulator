bbe6391398ca126ed28fd7bc6e935b5187419151
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-29 10:49:55 +0100

    added to client class

--- src/main/java/com/sparta/main/model/client/Client.java
+++ src/main/java/com/sparta/main/model/client/Client.java
@@ -1,4 +1,19 @@
 package com.sparta.main.model.client;
 
+
+import java.util.Random;
+
 public class Client {
+
+    String traineeType;
+    final int traineesRequired;
+
+    public Client() {
+        Random random = new Random();
+        this.traineesRequired = random.nextInt(15, 100);
+    }
+
+    public void makeRequirements() {
+
+    }
 }
