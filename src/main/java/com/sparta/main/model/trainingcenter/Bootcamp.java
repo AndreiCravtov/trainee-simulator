ef5f0cff9cc62f6ba6e85a7643cf31a100e8af81
lucacooperlindsay <lcooper-lindsay@spartaglobal.com>
2022-09-29 09:42:49 +0100

    bootcamp

--- src/main/java/com/sparta/main/model/trainingcenter/Bootcamp.java
+++ src/main/java/com/sparta/main/model/trainingcenter/Bootcamp.java
@@ -1,24 +1,37 @@
 package com.sparta.main.model.trainingcenter;
 
 import com.sparta.main.model.Trainee;
+import com.sparta.main.model.util.Timeable;
 
 public class Bootcamp extends TrainingCentre {
-    public Bootcamp(int centerID) {
-        super(centerID);
+    int closedCounter = 0;
+
+    public Bootcamp(int id, Timeable timekeeper) {
+        super(id,timekeeper);
     }
 
     @Override
     public boolean canBeClosed() {
+        //increment timer, if it is under 25
+        if(!timekeeper.inGlobalGracePeriod() &&
+                (timekeeper.getTime() - timeCreated) > LOCAL_GRACE_PERIOD &&
+                trainees.size() < 25){
+            closedCounter++;
+            return closedCounter == 3;
+        }
         return false;
     }
 
     @Override
     public boolean canAdd(Trainee trainee) {
-        return false;
+        return trainee != null &&
+                trainees.size() < 500 &&
+                !trainees.contains(trainee);
     }
 
     @Override
     public boolean addTrainee(Trainee trainee) {
-        return false;
+        if(!canAdd(trainee)) return false;
+        return trainees.add(trainee);
     }
 }
