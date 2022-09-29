14109dbe8bae5d1eef65b72aff46a8826922ef98
Kilmurray <66964622+MaddyKilmurray@users.noreply.github.com>
2022-09-29 09:52:57 +0100

    Waiting list classes rewritten

--- src/main/java/com/sparta/main/model/waitlist/WaitingList.java
+++ src/main/java/com/sparta/main/model/waitlist/WaitingList.java
@@ -3,19 +3,96 @@ package com.sparta.main.model.waitlist;
 import com.sparta.main.model.Trainee;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public interface WaitingList {
+public abstract class WaitingList {
 
-    public boolean addTrainee(Trainee trainee);
+    protected BlockingQueue<Trainee> newTraineeWaitingList;
+    protected BlockingQueue<Trainee> reassignWaitingList;
+
+    public boolean addTraineeByType(Trainee trainee, WaitingListType type) {
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList.offer(trainee);
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList.offer(trainee);
+        return false;
+    };
+
+    public boolean addTraineeByID(Trainee trainee, int typeID) {
+        WaitingListType type = WaitingListType.returnFromID(typeID);
+        if (type == null) throw new NullPointerException("Please enter a valid ID");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList.offer(trainee);
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList.offer(trainee);
+        return false;
+    };
 
     /**
-     * Returns the fist trainee in the waiting list.
+     * Returns the fist trainee in the new trainee waiting list.
      * @return the first trainee, if list not empty <br>
      * {@code null}, if list is empty
      */
-    public Trainee getFirstInQueue();
+    public Trainee getFirstInQueueByType(WaitingListType type) {
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) {
+            if (newTraineeWaitingList.size() == 0) {
+                return null;
+            }
+            return newTraineeWaitingList.poll();
+        }
+        if (type == WaitingListType.REASSIGN) {
+            if (reassignWaitingList.size() == 0) {
+                return null;
+            }
+            return reassignWaitingList.poll();
+        }
+        return null;
+    };
 
-    public BlockingQueue<Trainee> getWaitingList();
+    public Trainee getFirstInQueueByID(int typeID) {
+        WaitingListType type = WaitingListType.returnFromID(typeID);
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) {
+            if (newTraineeWaitingList.size() == 0) {
+                return null;
+            }
+            return newTraineeWaitingList.poll();
+        }
+        if (type == WaitingListType.REASSIGN) {
+            if (reassignWaitingList.size() == 0) {
+                return null;
+            }
+            return reassignWaitingList.poll();
+        }
+        return null;
+    };
 
-    public int sizeOfWaitingList();
+    public BlockingQueue<Trainee> getWaitingListByType(WaitingListType type) {
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList;
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList;
+        return null;
+    };
+
+    public BlockingQueue<Trainee> getWaitingListByID(int typeID) {
+        WaitingListType type = WaitingListType.returnFromID(typeID);
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList;
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList;
+        return null;
+    };
+
+    public int sizeOfWaitingListByType(WaitingListType type) {
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList.size();
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList.size();
+        return 0;
+    };
+
+    public int sizeOfWaitingListByID(int typeID) {
+        WaitingListType type = WaitingListType.returnFromID(typeID);
+        if (type == null) throw new NullPointerException("Waiting List Type cannot be null");
+        if (type == WaitingListType.NEWTRAINEE) return newTraineeWaitingList.size();
+        if (type == WaitingListType.REASSIGN) return reassignWaitingList.size();
+        return 0;
+    };
+
 }
