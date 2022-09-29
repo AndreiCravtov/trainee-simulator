68919818df274bebf54b30f149ce6453be18dde2
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-29 12:01:59 +0100

    Added a poisson distribution to the class

--- src/main/java/com/sparta/main/model/client/Poisson.java
+++ src/main/java/com/sparta/main/model/client/Poisson.java
@@ -9,7 +9,8 @@ public class Poisson {
         int sum = 0;
         double n_x = 1.0;
         do {
-            n_x = n_x * random.nextDouble();
+            n_x = n_x * random.nextDouble();  // essentially, a sum of n1 * n2 ... * n(n) < L, which means 1/mean -(ln(n1) + ln(n2) ...) < 1
+                                              // this works backwards on the lambda^x * e^(-lambda) * 1/x! formula
             sum++;
         } while (n_x > L);
         return sum - 1;
