dab1aa6504af7004cd3e2dbee6ed02ea731374f0
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-29 17:37:22 +0100

    added some code to the DisplayManager.java class

--- src/main/java/com/sparta/main/view/DisplayManager.java
+++ src/main/java/com/sparta/main/view/DisplayManager.java
@@ -1,4 +1,15 @@
 package com.sparta.main.view;
 
 public class DisplayManager {
+
+    public void displayMonths() {
+
+    }
+
+    public void displayIntro() {
+        System.out.println("============================== SPARTA SIMULATOR ==============================");
+        System.out.println("You can simulate sparta's business model of hiring trainees and assigning \n" +
+                "them to clients. ");
+        System.out.println("Enter the number of months you'd like to simulate");
+    }
 }
