f57396ab2cd8e987153617d00cbbb4e474b579d3
Zahir455 <smazahir5@gmail.com>
2022-09-29 11:30:49 +0100

    started client class

--- src/main/java/com/sparta/main/model/client/Client.java
+++ src/main/java/com/sparta/main/model/client/Client.java
@@ -1,16 +1,20 @@
 package com.sparta.main.model.client;
 
 
+import com.sparta.main.model.Course;
+
 import java.util.Random;
 
 public class Client {
 
     String traineeType;
     final int traineesRequired;
+    private final Course course;
 
-    public Client() {
+    public Client(Course course) {
+        this.course = course;
         Random random = new Random();
-        this.traineesRequired = random.nextInt(15, 100);// is this
+        this.traineesRequired = random.nextInt(15, 100);// may need a reasonable limit
     }
 
     public void makeRequirements() {
