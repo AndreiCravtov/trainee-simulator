f9a3366d9854b52ce07e9f01e44e0e430da2a5bd
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-30 12:26:31 +0100

    added a test class for BootcampTest

---
+++ src/test/java/com/sparta/main/model/trainingcenter/BootcampTest.java
@@ -0,0 +1,22 @@
+package com.sparta.main.model.trainingcenter;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BootcampTest {
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    @DisplayName("Functional Test 1")
+    void test1() {
+
+    }
+
+}
