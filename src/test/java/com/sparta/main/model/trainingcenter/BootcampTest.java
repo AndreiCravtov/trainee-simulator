1c404aaa40838c569369fb3fc670e847b4599689
Abdullah Navid <abdullahnavid5@gmail.com>
2022-09-30 14:38:28 +0100

    added a test for BootcampTest

--- src/test/java/com/sparta/main/model/trainingcenter/BootcampTest.java
+++ src/test/java/com/sparta/main/model/trainingcenter/BootcampTest.java
@@ -1,5 +1,9 @@
 package com.sparta.main.model.trainingcenter;
 
+import com.sparta.main.model.Trainee;
+import com.sparta.main.model.util.MonthTime;
+import com.sparta.main.model.util.Timeable;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,14 +12,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BootcampTest {
 
+    public MonthTime monthTime;
+    public Trainee testTrainee;
+    public Bootcamp bootcamp;
+    public Timeable timeable;
+
     @BeforeEach
     void setup() {
-
+        timeable = MonthTime.getInstance();
+        monthTime = MonthTime.getInstance();
+        testTrainee = new Trainee(monthTime);
+        bootcamp = new Bootcamp(timeable);
     }
 
     @Test
     @DisplayName("Functional Test 1")
     void test1() {
+        for (int i = 0; i < 499; i++) {
+            bootcamp.addTrainee(testTrainee);
+        }
+        Assertions.assertFalse(bootcamp.isFull());
 
     }
 
